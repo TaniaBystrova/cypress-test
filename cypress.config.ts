dd94f31e4cb24c3b9028c94f80bc2762bb04c5d5
Tania <bystrovat@procoders.tech>
2023-10-16 17:10:01 +0300

    hw cypress install

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  fixturesFolder: 'cypress/e2e/fixtures',
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:4200/',
+  },
+});
