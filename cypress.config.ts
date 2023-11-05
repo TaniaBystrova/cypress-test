00328ca9ba1fc579cf620b3de96d0e1377bdb011
Tania <bystrovat@procoders.tech>
2023-11-05 17:21:15 +0200

    cypress requests

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:4200/',
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
   },
 });
